05196500cfd0759448518d2edeef4fdcf37c2de1
ai-kafele <168393404+ai-kafele@users.noreply.github.com>
2025-11-03 14:52:46 -0500

    Renaming and Reconfiguration
    Renamed the class mdview to better convey its functionality. Reconfigured the class to be a non-owning view of a std::vector object, henceforth to be referred to as the "target".  The mdview constructor now takes as parameters a reference to the target as well as the extents of multidimensional view. mdview therefore a structured "manipulator" of the elements in the target. Also, the syntax of the interface was improved via the overloaded function call operator: ().

--- mdview.cpp
+++ mdview.cpp
@@ -20,7 +20,7 @@ the data() member function and the data member lumen respectively.
 #include <stdexcept>
 
 template <typename data_type> 
-class marray
+class mdview
 {
     //  DATA MEMBERS...  
     private:
@@ -28,18 +28,17 @@ class marray
         std::vector<std::size_t> extents;
         // std::vector holding the position to be accessed.
         std::vector<std::size_t> position;
-        // The number of extents... Can be interpreted as the order of a tensor.
+        // The number of extents... Should be interpreted as the dimensionality of the view.
         std::size_t order;
-        // The number of elements. Must remain constant across all reshapings and transpositions.
-        const std::size_t element_count;
         
     public:
-        // Linearly stored data of the multi-array.
-        std::vector<data_type> lumen;
+        // Reference to linear 'source' array.
+        std::vector<data_type>& source;
+        // The number of elements. Must remain constant across all reshapings and transpositions.
+        const std::size_t element_count;
 
     // MEMBER FUNCTIONS...
     private:
-
     // Checks for invalid access arguments.
         void check_valid_position(){
             if (extents.size() != position.size())
@@ -53,14 +52,11 @@ class marray
                 }
             }
 
-
     // Returns the linearized memory index of the referenced element: (a,b,c,...).
         std::size_t linearize(){
-
             // Initialize accumulator values. 
             std::size_t pos_product = 1;
             std::size_t linear_index = 0;
-
             // Calculates the linear index given the data stored in the 'position' vector.
             for(std::size_t k = 0; k != order; k++){
                 for(std::size_t j = k + 1; j != order; j++){
@@ -72,39 +68,29 @@ class marray
             return linear_index;
         }
 
-    //Extents extraction from parameter list: "std::size_t... dimensions".
+    // Extents extraction from parameter list: "std::size_t... dimensions".
         template<typename... index_type>
-        std::vector<std::size_t> extractor(const index_type&... args){
+        std::vector<std::size_t> extractor(index_type... args){
             return {std::forward<std::size_t>(args)...};
         }
 
     public:
-    
-    // Returns a pointer to the location in memory corresponding to the requested position coordinates.
         template<typename... index_type>
-        auto data(const index_type&... coordinates){
-            
+        data_type& operator()(index_type... coordinates){
             // Uses the extractor member function to unfold the pack and apply the coordinates to the positon vector.
             position = extractor(coordinates...);
-            
             // Argument check...
             check_valid_position();
-
             // Returns the linear index of the coordinates.
             std::size_t offset = linearize();
-            
-            // Returns an iterator to the position corresponding to the "coordinates"
-            return lumen.begin() + offset;
+            // Returns a pointer to the location in memory corresponding to the requested position coordinates.
+            return *(source.begin() + offset);
         }
-
+        
     // Constructors...
         template<typename... index_type>
-        marray(const index_type&... dimensions): extents(extractor(dimensions...)), order(sizeof...(dimensions)), element_count((dimensions * ...)){
-            // Set additional capacity for 'lumen' for potential accumulation operations.           
-            std::size_t padding = *std::max_element(extents.begin(),extents.end());
-            lumen.resize(element_count);
-            lumen.reserve(element_count + padding);
-            
+        mdview(std::vector<data_type>& source, index_type... dimensions): source(source), extents(extractor(dimensions...)), order(sizeof...(dimensions)), element_count((dimensions * ...)){
+            // Set additional capacity for 'source' for potential accumulation operations.           
             // Sets size of the position vector.
             position.resize(order);
         }
